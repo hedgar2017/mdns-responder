eef0c10142f1392c78a2e77e6758e08161422fb9
Gleb Pomykalov <gleb@lancastr.com>
2018-12-20 18:48:02 +0300

    Fix deprecation warning

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ extern crate rand;
 extern crate tokio_core as tokio;
 
 use dns_parser::Name;
-use futures::{BoxFuture, Future};
+use futures::{Future};
 use futures::sync::mpsc;
 use std::io;
 use std::thread;
@@ -51,7 +51,7 @@ pub struct Service {
 }
 
 impl Responder {
-    fn setup_core() -> io::Result<(Core, BoxFuture<(), io::Error>, Responder)> {
+    fn setup_core() -> io::Result<(Core, Box<dyn Future<Item=(), Error=io::Error>>, Responder)> {
         let core = Core::new()?;
         let (responder, task) = Self::with_handle(&core.handle())?;
         Ok((core, task, responder))
@@ -85,7 +85,7 @@ impl Responder {
         Ok(responder)
     }
 
-    pub fn with_handle(handle: &Handle) -> io::Result<(Responder, BoxFuture<(), io::Error>)> {
+    pub fn with_handle(handle: &Handle) -> io::Result<(Responder, Box<dyn Future<Item=(), Error=io::Error>>)> {
         let mut hostname = net::gethostname()?;
         if !hostname.ends_with(".local") {
             hostname.push_str(".local");
@@ -96,16 +96,16 @@ impl Responder {
         let v4 = FSM::<Inet>::new(handle, &services);
         let v6 = FSM::<Inet6>::new(handle, &services);
 
-        let (task, commands) = match (v4, v6) {
+        let (task, commands): (Box<dyn Future<Item=(), Error=io::Error>>, _) = match (v4, v6) {
             (Ok((v4_task, v4_command)), Ok((v6_task, v6_command))) => {
-                let task = v4_task.join(v6_task).map(|((),())| ()).boxed();
+                let task = Box::new(v4_task.join(v6_task).map(|((),())| ()));
                 let commands = vec![v4_command, v6_command];
                 (task, commands)
             }
 
             (Ok((v4_task, v4_command)), Err(err)) => {
                 warn!("Failed to register IPv6 receiver: {:?}", err);
-                (v4_task.boxed(), vec![v4_command])
+                (Box::new(v4_task), vec![v4_command])
             }
 
             (Err(err), _) => return Err(err),
@@ -118,7 +118,7 @@ impl Responder {
             shutdown: Arc::new(Shutdown(commands)),
         };
 
-        Ok((responder, task.boxed()))
+        Ok((responder, Box::new(task)))
     }
 }
 
